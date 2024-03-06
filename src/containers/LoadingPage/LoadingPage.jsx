53933f52e5baf62d981f98b674ee95b23a942411
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-03-06 22:57:54 +0530

    structuring

---
+++ src/containers/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from './Images/logo.png';
+import heroimg from './Images/Hero Image.png';
+
+function LoadingPage() {
+  return (
+    <div className='Loadingpage'>
+        <img src={logo} className="logo" alt="logo" />
+        <h1 className="herotext">Farm fresh for all</h1>
+        <img src={heroimg} className="heroimage" alt="main" />
+        <div className="getbutton">
+        <p>Get Started</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default LoadingPage
