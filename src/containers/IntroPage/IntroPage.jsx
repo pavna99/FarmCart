2392749054a1f31f56ac6a42db09d681a82ff1f1
Paveena <pavnaptr@gmail.com>
2024-03-07 11:25:27 +0530

    updates

--- src/containers/IntroPage/IntroPage.jsx
+++ src/containers/IntroPage/IntroPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import image from '../../Images/image 12.png';
+import './IntroPage.css';
+import card1 from '../../Images/card.svg'; 
 import { Link } from 'react-router-dom';
 
 function IntroPage() {
   return (  
     <div className='IntroPage'>
-       <img src={image} className='Main' alt="screen"/>
+       <div className="card-container">
+            <div className='onetext'>
+              <h2 style={{color:'#7519EB'}}>FARM FRESH FOR <br></br>ALL</h2>
+              We love to solve sfreal world problems with design solutions that are magically simple and useable. 
+            </div>
+          
+      </div>
        <Link to="/signup">
           <div className="getbutton">
             <p>hohosignup</p>
