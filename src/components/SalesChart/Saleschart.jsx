5d675ba580ac24ce558ccae0bd2c8bd031ddc38b
Paveena <pavnaptr@gmail.com>
2024-04-16 20:47:41 +0530

    chart

--- src/components/SalesChart/Saleschart.jsx
+++ src/components/SalesChart/Saleschart.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from "react";
 import {
   BarChart, XAxis,
-  Bar,Rectangle,Cell,
-  ResponsiveContainer,
+  Bar, Cell,
+  ResponsiveContainer,Legend
 } from "recharts";
-
+import './Saleschart.css'
 const data = [
   {
     name: "A",
@@ -50,7 +50,6 @@ const data = [
   },
 ];
 
-
 const getCustomShapePath = (x, y, width, height, borderRadius) => {
   const radius = borderRadius || 5; // Default border radius
   
@@ -83,24 +82,32 @@ const CustomShape = (props) => {
 
   return <path d={getCustomShapePath(x, y, halfWidth, height)} stroke="none" fill={fill} />;
 };
+
+const maxUV = Math.max(...data.map((item) => item.uv));
+
 export default class SalesChart extends PureComponent {
   static demoUrl = "https://codesandbox.io/s/tiny-bar-chart-35meb";
 
   render() {
-    
-   
-
     return (
+     
       <ResponsiveContainer className="chart" width="74%" height="25%">
         <BarChart width={30} height={40} data={data}>
           <XAxis dataKey="name" axisLine={false} tick={true} />
           
-          <Bar dataKey="uv" fill="rgba(117, 25, 235, 0.3)" shape={<CustomShape />}  
-            />
-          
+          <Bar dataKey="uv" fill="rgba(117, 25, 235, 0.3)" shape={<CustomShape />}>
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                cursor="pointer"
+                fill={entry.uv === maxUV ? '#7519EB' : 'rgba(117, 25, 235, 0.3)'}
+              />
+            ))}
+          </Bar>
           
         </BarChart>
       </ResponsiveContainer>
+      
     );
   }
 }
